5388781c4cabb3214ab43f10b37782ef0e9a6d24
Yessenia Rodriguez <32173043+yessenia4@users.noreply.github.com>
2018-05-06 18:57:30 -0500

    Updated

--- ImageCapture/src/recog/Features.java
+++ ImageCapture/src/recog/Features.java
@@ -10,8 +10,36 @@ package recog;
  * @author xwc981
  */
 public class Features {
+    private double[] feat;
     
-    public double compareFeatures(double[] features1, double[] features2, int noToCompare)
+    public double[] getFeatures(){
+        return feat;
+    }
+    public void setFeatures(double[] features){
+        feat = features;
+    }
+    
+    //Constructor
+    public Features(double[] features){
+        feat = features;
+    }
+    
+    //returns true if find a match
+    public boolean compareFeatures(double[] features, int noToCompare, ArrayList<double[]> myFeatureList){
+        boolean match = false;
+        
+        for (double[] temp : myFeatureList) {
+            double difference = compare2Features(features, temp, noToCompare);
+            if(difference <= 20){
+                match = true;
+                break;
+            }
+        }
+        return match;
+    }
+    
+    //gets difference between two feature arrays
+    public double compare2Features(double[] features1, double[] features2, int noToCompare)
     {
         //size will come from the slider
         double sum=0;
