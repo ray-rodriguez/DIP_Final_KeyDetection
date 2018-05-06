1f042c14471514d26b519f1229ecd285849cd887
Yessenia Rodriguez <32173043+yessenia4@users.noreply.github.com>
2018-05-06 18:33:32 -0500

    Added Operation to compare features.

--- ImageCapture/src/recog/Features.java
+++ ImageCapture/src/recog/Features.java
@@ -10,6 +10,14 @@ package recog;
  * @author xwc981
  */
 public class Features {
-    // features for the key
     
+    public double compareFeatures(double[] features1, double[] features2, int noToCompare)
+    {
+        //size will come from the slider
+        double sum=0;
+        for (int i = 0; i < noToCompare; i++)
+            sum += ((features1[i] - features2[i]) * (features1[i] - features2[i]));
+        
+        return (double) Math.sqrt(sum);
+    }
 }
